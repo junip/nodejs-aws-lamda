439b955de97e8e50a7b37cc72d2f62ba0f041817
Junip Dewan <junipd2@gmail.com>
2020-04-05 17:18:20 +0530

    :rocket: First commit

---
+++ handler.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports.sendReminderDaily = (event, context, callback) => {
+  var AWS = require('aws-sdk');
+  AWS.config.update({region:'us-east-1'});
+  var ses = new AWS.SES();
+  var fs = require('fs');
+
+  var emailHtml = fs.readFileSync('./dailyReminder.html', 'utf-8');
+
+  var toAndFromAdress = 'junipd2@gmail.com'
+  var params = {
+      Destination: {
+          ToAddresses: [toAndFromAdress]
+      },
+      Message: {
+          Body: {
+              Html: {
+                  Charset: "UTF-8", 
+                  Data: emailHtml
+              }, 
+              Text: {
+                  Charset: "UTF-8", 
+                  Data: "Remember to continue helping the Woof Garden in your Pluralsight course!"
+              }
+          }, 
+          Subject: {
+              Charset: "UTF-8", 
+              Data: "Woof Garden Reminder"
+          }
+      },
+      ReplyToAddresses: [toAndFromAdress],
+      Source: toAndFromAdress, 
+  };
+
+  ses.sendEmail(params, function(err, data) {
+      // an error occurred
+      if (err) console.log(err, err.stack); 
+      // successful response
+      else callback(null, data);
+  }); 
+};
